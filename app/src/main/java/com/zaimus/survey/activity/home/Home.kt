8fedb4f76bc5411fa7af3f97e070bccd0b63e04c
Sanju Sabu <sanju.sabu@mobatia.com>
2023-06-23 17:54:43 +0530

    Changes

---
+++ app/src/main/java/com/zaimus/survey/activity/home/Home.kt
@@ -0,0 +1,12 @@
+package com.zaimus.survey.activity.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zaimus.survey.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/zaimus/survey/constant/custom_view/CustomFontTextView.kt
@@ -0,0 +1,30 @@
+package com.zaimus.survey.constant.custom_view
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import android.widget.TextView
+import com.zaimus.survey.R
+
+
+class CustomFontTextView:androidx.appcompat.widget.AppCompatTextView {
+
+    constructor(context: Context) : super(context) {
+        val type = Typeface.createFromAsset(context.assets, "fonts/Lato_Light.ttf")
+        this.typeface = type
+        this.setTextColor(context.resources.getColor(R.color.black))
+//        this.textSize = 26F
+    }
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet) {
+        val type = Typeface.createFromAsset(context.assets, "fonts/Lato_Light.ttf")
+        this.typeface = type
+        this.setTextColor(context.resources.getColor(R.color.black))
+//        this.textSize = 26F
+    }
+    constructor(context: Context, attributeSet: AttributeSet, defStyleAttr: Int) : super(context, attributeSet, defStyleAttr) {
+        val type = Typeface.createFromAsset(context.assets, "fonts/Lato_Light.ttf")
+        this.typeface = type
+        this.setTextColor(context.resources.getColor(R.color.black))
+//        this.textSize = 26F
+    }
+}
